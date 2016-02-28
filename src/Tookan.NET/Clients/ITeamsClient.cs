fb94654b762639009c29e71f78b78d7c81e5ec39
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2016-02-28 12:55:48 +0530

    Added Team, ITeam, ITeamsClient

---
+++ src/Tookan.NET/Clients/ITeamsClient.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using Tookan.NET.Core;
+
+namespace Tookan.NET.Clients
+{
+    public interface ITeamsClient
+    {
+        Task<Team> GetTeamAsync(string accessToken);
+    }
+}
---
+++ src/Tookan.NET/Core/Team.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace Tookan.NET.Core
+{
+    public class Team : ITeam
+    {
+        private List<Agent> _fleets;
+        public string TeamName { get; set; }
+        public int TeamId { get; set; }
+
+        public List<Agent> Fleets
+        {
+            get { return _fleets ?? (_fleets = new List<Agent>()); }
+            set { _fleets = value; }
+        }
+    }
+}
