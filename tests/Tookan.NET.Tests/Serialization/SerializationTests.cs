8f1c1a40e1630b3757339800607408631fe1f879
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2016-02-28 10:48:40 +0530

    Added more tests, for primitive types

--- tests/Tookan.NET.Tests/Serialization/SerializationTests.cs
+++ tests/Tookan.NET.Tests/Serialization/SerializationTests.cs
@@ -1,4 +1,5 @@
-﻿using Tookan.NET.Serialization;
+﻿using System;
+using Tookan.NET.Serialization;
 using Xunit;
 
 namespace Tookan.NET.Tests.Serialization
@@ -10,6 +11,13 @@ namespace Tookan.NET.Tests.Serialization
         private class TestClass
         {
             public int GhostTownsCount { get; set; }
+        }
+
+        private enum TestEnum
+        {
+            None = 0,
+            TwoDays = 1,
+            FiveDays = 2
         }
 
         [Fact]
@@ -50,6 +58,112 @@ namespace Tookan.NET.Tests.Serialization
 
             // Assert
             Assert.Equal(5, actual);
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData((byte)1)]
+        [InlineData((sbyte)1)]
+        [InlineData((short)1)]
+        [InlineData((ushort)1)]
+        [InlineData((long)1)]
+        [InlineData((ulong)1)]
+        [InlineData(1U)]
+        [InlineData(1D)]
+        [InlineData(1F)]
+        [InlineData(true)]
+        [InlineData('n')]
+        [InlineData("Some random string")]
+        public void ConsecutiveSerializationAndDeserializationOfPrimitiveTypesIsIdentityFunction(object input)
+        {
+            // Arrange
+            var expected = input;
+
+            // Act
+            var actual = (object) SerializeAndDeserialize((dynamic) input);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void ConsecutiveSerializationAndDeserializationOfGuidIsIdentityFunction()
+        {
+            var input = Guid.NewGuid();
+
+            // Arrange
+            var expected = input;
+
+            // Act
+            var actual = SerializeAndDeserialize(input);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void ConsecutiveSerializationAndDeserializationOfDateTimeIsIdentityFunction()
+        {
+            var input = DateTime.Now;
+
+            // Arrange
+            var expected = input;
+
+            // Act
+            var actual = SerializeAndDeserialize(input);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void ConsecutiveSerializationAndDeserializationOfDateTimeOffsetIsIdentityFunction()
+        {
+            var input = DateTimeOffset.UtcNow;
+
+            // Arrange
+            var expected = input;
+
+            // Act
+            var actual = SerializeAndDeserialize(input);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void ConsecutiveSerializationAndDeserializationOfTimeSpanIsIdentityFunction()
+        {
+            var input = TimeSpan.FromMinutes(12);
+
+            // Arrange
+            var expected = input;
+
+            // Act
+            var actual = SerializeAndDeserialize(input);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void ConsecutiveSerializationAndDeserializationOfEnumIsIdentityFunction()
+        {
+            var input = TestEnum.TwoDays;
+
+            // Arrange
+            var expected = input;
+
+            // Act
+            var actual = SerializeAndDeserialize(input);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        private T SerializeAndDeserialize<T>(T input)
+        {
+            return _service.Deserialize<T>(_service.Serialize(input));
         }
     }
 }
