e1d714dbace81c64889ded71412e30085483b1a0
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2016-02-28 13:35:06 +0530

    Added Task History

---
+++ src/Tookan.NET/Core/TaskHistory.cs
@@ -0,0 +1,15 @@
+﻿namespace Tookan.NET.Core
+{
+    public class TaskHistory
+    {
+        public int Id { get; set; }
+        public int JobId { get; set; }
+        public int FleetId { get; set; }
+        public string FleetName { get; set; }
+        public string Latitude { get; set; }
+        public string Longitude { get; set; }
+        public string Type { get; set; }
+        public string Description { get; set; }
+        public string CreationDateTime { get; set; }
+    }
+}
